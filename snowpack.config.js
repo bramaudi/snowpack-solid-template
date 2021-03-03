027f1cb6c4852df75fc2f43a72a8c9f12158cba5
Brama Udi <bramaudi@users.noreply.github.com>
2021-03-03 18:40:10 +0700

    Enable SPA fallback

--- snowpack.config.js
+++ snowpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {"match": "routes", "src": ".*", "dest": "/"},
   ],
   optimize: {
     /* Example: Bundle your final build: */
